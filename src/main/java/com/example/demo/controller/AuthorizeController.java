2e3775e20bdb1ae82c2d7ed53cfdb48f270dd332
whydoyouknowme <1464184895@qq.com>
2019-08-06 21:03:14 +0800

    08.06

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -24,7 +26,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                            @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -33,8 +36,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accesstoken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUer(accesstoken);
-        System.out.println(user.getName());
-        return "index";
+        if(user !=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+            //登陆成功，写cokie和session
+        }else {
+            return "redirect:/";
+            //重新登陆
+        }
     }
 
 }
